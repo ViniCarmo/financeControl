de54b2040232ef531ea8726212bf52879335ec90
ViniCarmo <vinicius.carmo1106@gmail.com>
2026-01-19 14:37:20 -0300

    create controller

--- src/main/java/com/vinicius/finance_api/Controller/TransactionController.java
+++ src/main/java/com/vinicius/finance_api/Controller/TransactionController.java
@@ -1,7 +1,39 @@
 package com.vinicius.finance_api.Controller;
 
+import com.vinicius.finance_api.Dto.TransactionRequestDto;
+import com.vinicius.finance_api.Service.TransactionService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import java.util.List;
+
+@RestController("/transactions")
 public class TransactionController {
+    private final TransactionService transactionService;
+
+    public TransactionController(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> saveTransaction(TransactionRequestDto transactionRequestDto) {
+        transactionService.saveTransaction(transactionRequestDto);
+        return ResponseEntity.ok().build();
+    }
+    @DeleteMapping
+    public ResponseEntity<Void> deleteTransaction(Integer id) {
+        transactionService.deleteTransactionById(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TransactionRequestDto>> getAllTransactions() {
+        transactionService.getAllTransactions();
+        return ResponseEntity.ok().build();
+    }
+
+
 }
