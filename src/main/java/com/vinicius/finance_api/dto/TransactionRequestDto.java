c6324aea978ea8cdea26c7d195a1b26ecbe7d1a5
ViniCarmo <vinicius.carmo1106@gmail.com>
2026-02-10 21:55:27 -0300

    add validation rules to request DTOs

--- src/main/java/com/vinicius/finance_api/dto/TransactionRequestDto.java
+++ src/main/java/com/vinicius/finance_api/dto/TransactionRequestDto.java
@@ -4,11 +4,14 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.vinicius.finance_api.enums.TransactionType;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 
 public record TransactionRequestDto(
         @NotNull(message = "Value cannot be null")
+        @Positive(message = "Value must be greater than zero")
         @JsonProperty("value")
         Double value,
 
@@ -21,10 +24,12 @@ public record TransactionRequestDto(
         LocalDate date,
 
         @NotBlank(message = "Description cannot be blank")
+        @Size(max = 255, message = "Description must have at most 255 characters")
         @JsonProperty("description")
         String description,
 
         @NotNull(message = "User ID cannot be null")
+        @Positive(message = "User ID must be a positive number")
         @JsonProperty("userId")
         Integer userId
 ) {
--- src/main/java/com/vinicius/finance_api/dto/UserRequestDto.java
+++ src/main/java/com/vinicius/finance_api/dto/UserRequestDto.java
@@ -1,18 +1,24 @@
 package com.vinicius.finance_api.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public record UserRequestDto(
-                             @NotNull(message = "Username cannot be null")
-                             @JsonProperty("username")
-                             String username,
+        @NotBlank(message = "Username cannot be blank")
+        @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
+        @JsonProperty("username")
+        String username,
 
-                             @NotNull(message = "Email cannot be null")
-                             @JsonProperty("email")
-                             String email,
+        @NotBlank(message = "Email cannot be blank")
+        @Email(message = "Email must be valid")
+        @JsonProperty("email")
+        String email,
 
-                             @NotNull(message = "Password cannot be null")
-                             @JsonProperty("password")
-                             String password) {
+        @NotBlank(message = "Password cannot be blank")
+        @Size(min = 6, max = 100, message = "Password must have at least 6 characters")
+        @JsonProperty("password")
+        String password) {
 }
